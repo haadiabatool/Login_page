a1b6752b6f310604ca321a708e5f05cfac259d5a
Haadia Batool <haadiabatool971@gmail.com>
2026-02-26 12:44:32 +0500

    New Branch

--- index.js
+++ index.js
@@ -17,4 +17,23 @@ document.addEventListener("DOMContentLoaded", function () {
         signupCard.classList.toggle('hidden');
         loginCard.classList.toggle('hidden');
     });
+
+    // For THeme button
+    const toggleBtn=document.querySelector('.themeBtn');
+
+    if(localStorage.getItem('theme')=='dark'){
+        document.body.classList.add('dark');
+        toggleBtn.textContent='☀️'
+    }
+    toggleBtn.addEventListener('click',()=>{
+        document.body.classList.toggle('dark');
+
+        if(document.body.classList.contains('dark')){
+            localStorage.setItem('theme','dark');
+            toggleBtn.textContent="☀️";
+        }else{
+            localStorage.setItem('theme', 'light');
+            toggleBtn.textContent = "🌙";
+        }
+    });
 });
